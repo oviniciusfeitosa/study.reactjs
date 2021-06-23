d15e216b0d4708ff47621c3f41a38b0b4a601fca
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2021-06-22 23:36:08 -0300

    ✨ Add Category Page
    Co-authored-by: Gabriel Lopes <gblsilvalopes@gmail.com>

---
+++ src/pages/Category.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { CategoryForm } from "../components/CategoryForm";
+
+export const Category = () => {
+  return (
+    <>
+      <h1>Category Page</h1>
+      <CategoryForm />
+    </>
+  );
+};
